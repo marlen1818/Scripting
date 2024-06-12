4cdc35c29ba770952fe013513b66810e10254655
marlen1818 <ra.marlen2023@gmail.com>
2024-06-12 01:57:16 +0000

    End session2

---
+++ session/built-in.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+
+echo $0
+echo "$1 - $2 - $3 ... $9"
+read -p "What is your name?: " name
+echo "Hello there $name"
---
+++ session/variables.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+name="Marlen Ramanov"
+echo "Hello there, my name is $(whoami)"
+echo "We are currently located here $(pwd)"
+echo "This is script is begin developed by $name"
+
+today="$(date)"
+echo $today
